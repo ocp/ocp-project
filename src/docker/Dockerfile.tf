9992147dd46f6d79815f82fba9a646ad3ab6fa96
jhrr <jhrr@users.noreply.github.com>
2025-12-02 11:56:52 +0000

    Also build iamlive manually

--- src/docker/Dockerfile.tf
+++ src/docker/Dockerfile.tf
@@ -2,20 +2,30 @@
 # vim: set ft=dockerfile:
 
 # Must be declared at global scope.
-ARG IAMLIVE=1.1.27
 ARG TERRAFORM=1.14.0
-ARG TERRAGRUNT=0.93.11
 
-FROM ghcr.io/iann0036/iamlive:${IAMLIVE} AS iamlive
 FROM hashicorp/terraform:${TERRAFORM} AS terraform
 
-FROM debian:bookworm-slim AS terragrunt
+FROM debian:trixie-slim AS base
 RUN apt-get update && \
     apt-get install -y --no-install-recommends \
     ca-certificates \
     curl && \
     apt-get clean
+
+FROM base AS iamlive
 ARG TARGETARCH
+ARG IAMLIVE=v1.1.27
+RUN curl -Lo \
+  ./iamlive.tar.gz \
+  https://github.com/iann0036/iamlive/releases/download/$IAMLIVE/iamlive-$IAMLIVE-$(uname)-$TARGETARCH.tar.gz
+RUN tar -xzf iamlive.tar.gz
+RUN chmod +x iamlive.tar.gz
+RUN mv iamlive /usr/local/bin/iamlive
+
+FROM base AS terragrunt
+ARG TARGETARCH
+ARG TERRAGRUNT=0.93.11
 RUN curl -Lo \
   ./terragrunt \
   https://github.com/gruntwork-io/terragrunt/releases/download/$TERRAGRUNT/terragrunt_$(uname)_$TARGETARCH
@@ -33,7 +43,7 @@ RUN apt-get update && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 COPY --from=iamlive /usr/local/bin/iamlive /usr/local/bin/iamlive
-COPY --from=terraform /usr/local/bin/terraform /usr/local/bin/terraform
+COPY --from=terraform /bin/terraform /usr/local/bin/terraform
 COPY --from=terragrunt /usr/local/bin/terragrunt /usr/local/bin/terragrunt
 WORKDIR /opt/tf
 ENV TF_CLI_CONFIG_FILE=/opt/tf/.terraformrc
